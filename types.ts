b3e7ac0da7798c1242c6a7f57aa4c491fa65a855
Yanko Atanasov Aleksandrov <superactro@gmail.com>
2017-10-26 14:29:28 +0300

    Initial Commit

---
+++ types.ts
@@ -0,0 +1,41 @@
+console.log('Hello');
+
+// Optional Static Types
+
+var counter; // (any) type
+var counter1 = 0; // number (inferred)
+var counter2: number; // number
+var counter3: number = 0; // number
+
+// Boolean
+
+var isDone: boolean = false;
+
+// Number
+
+var height: number = 6;
+
+// String
+
+var name1: string = 'Yanko';
+
+// Array
+
+var list: number[] = [1, 2, 3];
+
+var notSure: any = 4;
+notSure = 'Can be string';
+notSure = 1;
+
+// Void
+
+function wantUser(someFunc: () => {}): void {
+}
+
+// Typeof
+
+var x: any = {};
+if (typeof x === 'string') {
+
+}
+
