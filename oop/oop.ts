03e6386fdcb5c829685b09bcf50c68a88f3ddc93
Yanko Atanasov Aleksandrov <superactro@gmail.com>
2017-10-27 13:06:05 +0300

    Stuff

--- oop/oop.ts
+++ oop/oop.ts
@@ -3,23 +3,32 @@ console.log('Hello');
 // Class Person
 
 class Person {
-    public name: string;
-    public surname: string;
-    public email: Email;
+    private name: string;
+    private surname: string;
+    private email: Email;
 
     constructor(name: string, surname: string, email: Email) {
         this.name = name;
         this.surname = surname;
         this.email = email;
-
-
     }
 
     getEmail() {
         return this.email.get();
     }
+}
 
+class Teacher extends Person {
+    public teach() {
+        console.log('Super get parent email from teacher: ', super.getEmail());
+        console.log('Hello Noobs, I\'m your Teacher!');
+    }
+}
 
+class Direktor extends Teacher {
+    public sayKur() {
+        console.log('Kur !');
+    }
 }
 
 class Email {
@@ -48,3 +57,14 @@ let myEmail = new Email('superactro@gmail.com');
 let myPerson = new Person('Yanko', 'Alexandrov', myEmail);
 
 console.log(myPerson.getEmail());
+
+
+let myTeacher = new Teacher('Dr.', 'House', myEmail);
+myTeacher.teach();
+myTeacher.getEmail();
+
+let myDirector = new Direktor('Boss', 'Bos', myEmail);
+myDirector.sayKur();
+myDirector.getEmail();
+
+// Mixins
